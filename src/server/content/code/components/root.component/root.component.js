a906a91e3362cc144a2513f8cbe9a7f7f242cd8a
Willi Linke <linke.willi@googlemail.com>
2017-06-30 12:29:53 +0200

    setup service architecture

--- src/server/content/code/components/root.component/root.component.js
+++ src/server/content/code/components/root.component/root.component.js
@@ -2,12 +2,44 @@
  * Created by willi.linke on 30.06.2017
  * */
 
+(function() {
+    "use strict";
 
-angular.module('application')
-    .component('root', {
-        templateUrl: 'code/components/root.component/root.template.html',
-        controller: function () {
+    angular.module('application')
+        .component('root', {
+            templateUrl: 'code/components/root.component/root.template.html',
+            controller: RootCtrl,
+            bindings: {
 
-        }
+            }
 
-    });
+        });
+
+
+    RootCtrl.$inject = [ '$scope', 'uiGmapGoogleMapApi'];
+
+    function RootCtrl($scope, uiGmapGoogleMapApi) {
+        "use strict";
+        let $ctrl = this;
+
+
+        $ctrl.map = {
+            centre:   {
+                latitude: 44.2126995,
+                longitude: -100.2471641
+            },
+            zoom: 3 ,
+            options : {
+                scrollwheel: false
+            }
+        };
+
+
+        uiGmapGoogleMapApi.then(function(maps) {
+            console.log('Maps loaded')
+        });
+
+
+    }
+}());
+
